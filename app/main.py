db39e8edc80a24a70dd1fded234ce8a586fefba1
ArtemSam23 <artemsam23@gmail.com>
2022-02-15 16:34:39 +0300

    refactor: main.py

--- app/main.py
+++ app/main.py
@@ -2,15 +2,14 @@ from fastapi import FastAPI
 from mangum import Mangum
 
 app = FastAPI()
+handler = Mangum(app)
 
 
 @app.get("/")
-async def root():
+def root():
     return {"message": "Hello World"}
 
 
 @app.get("/hello/{name}")
-async def say_hello(name: str):
+def say_hello(name: str):
     return {"message": f"Hello {name}"}
-
-handler = Mangum(app)
