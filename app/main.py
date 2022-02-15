ce7e90a20c5be20639bb0ea94fe04e424d10e0f2
ArtemSam23 <artemsam23@gmail.com>
2022-02-15 16:04:41 +0300

    refactor: main.py and requirements.txt

--- app/main.py
+++ app/main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
+from mangum import Mangum
+
 app = FastAPI()
 
 
 @app.get("/")
-async def lambda_handler():
+async def root():
     return {"message": "Hello World"}
+
+
+@app.get("/hello/{name}")
+async def say_hello(name: str):
+    return {"message": f"Hello {name}"}
+
+handler = Mangum(app)
